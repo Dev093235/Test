70ddab1b7a99bf749de59a071591f27aabe28bb3
Dev093235 <dev431140@gmail.com>
2025-05-23 14:42:19 +0530

    Update pair.js

--- Priyansh/commands/pair.js
+++ Priyansh/commands/pair.js
@@ -1,51 +1,58 @@
+const axios = global.nodemodule["axios"];
+const fs = global.nodemodule["fs-extra"];
+
 module.exports.config = {
   name: "pair",
-  version: "1.0.0", 
+  version: "2.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
-  description: "0Pairing",
-  commandCategory: "Love", 
-  usages: "pair", 
-  cooldowns: 0
+  credits: "Rudra (Stylish Hinglish Edition)",
+  description: "Create romantic pairing with full emoji + anime feel",
+  commandCategory: "Love",
+  usages: "pair",
+  cooldowns: 5
 };
-module.exports.run = async function({ api, event, args, Users, Threads, Currencies }) {
-        const axios = global.nodemodule["axios"];
-        const fs = global.nodemodule["fs-extra"];
-        // var data = await Currencies.getData(event.senderID);
-        var money = data.money
-        if(money < 1) api.sendMessage("You need 500 USD for 1 pairing, please use ${global.config.PREFIX}work to received money or ask for admin bot!\n🤑Theres something new to eat🤑",event.threadID,event.messageID)
-        else {
-         var tl = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        let dataa = await api.getUserInfo(event.senderID);
-        let namee = await dataa[event.senderID].name
-        let loz = await api.getThreadInfo(event.threadID);
-        var emoji = loz.participantIDs;
-        var id = emoji[Math.floor(Math.random() * emoji.length)];
-        let data = await api.getUserInfo(id);
-        let name = await data[id].name
-        var arraytag = [];
-                arraytag.push({id: event.senderID, tag: namee});
-                arraytag.push({id: id, tag: name});
-        // api.changeNickname(`😘👉🔐🔐 ${name} Property 🔐🔐👈😘`, event.threadID, event.senderID);
-        // api.changeNickname(`😘👉🔐🔐 ${namee} Property🔐🔐👈😘`, event.threadID, id);
-        var sex = await data[id].gender;
-        var gender = sex == 2 ? "Male🧑" : sex == 1 ? "Female👩‍🦰" : "Trần Đức Bo";
-        // Currencies.setData(event.senderID, options = {money: money - 500})
-        let Avatar = (await axios.get( `https://graph.facebook.com/${id}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-            fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
 
-          let gifLove = (await axios.get( `https://i.imgur.com/vcydK3t.gif`, { responseType: "arraybuffer" } )).data; 
-              fs.writeFileSync( __dirname + "/cache/giflove.png", Buffer.from(gifLove, "utf-8") );
-          
-        let Avatar2 = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
-            fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
-        var imglove = [];
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
-              imglove.push(fs.createReadStream(__dirname + "/cache/avt2.png"));
-        var msg = {body: `🥰Successful pairing!\n💌Wish you two hundred years of happiness\nyour partner is of the same gender: ${gender}\nDual ratio: ${tle}\n`+namee+" "+"❤️"+" "+name, mentions: arraytag, attachment: imglove}
-        // var msg = {body: `🥰Successful pairing!\n💌Wish you two hundred years of happiness\n💕Double ratio: ${tle}%\n`+namee+" "+"💓"+" "+name, mentions: arraytag, attachment: imglove}  
-         return api.sendMessage(msg, event.threadID, event.messageID)
-      }
-}
+module.exports.run = async function({ api, event }) {
+  const { threadID, senderID, messageID } = event;
+
+  const loveRate = ['21%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', '0%', '48%'];
+  const compatibility = loveRate[Math.floor(Math.random() * loveRate.length)];
+
+  const threadInfo = await api.getThreadInfo(threadID);
+  const members = threadInfo.participantIDs.filter(id => id !== senderID);
+  const partnerID = members[Math.floor(Math.random() * members.length)];
+
+  const user1 = await api.getUserInfo(senderID);
+  const user2 = await api.getUserInfo(partnerID);
+
+  const name1 = user1[senderID].name;
+  const name2 = user2[partnerID].name;
+  const gender = user2[partnerID].gender === 2 ? "Male 🧑" : user2[partnerID].gender === 1 ? "Female 👩" : "Unknown";
+
+  const tagUsers = [
+    { id: senderID, tag: name1 },
+    { id: partnerID, tag: name2 }
+  ];
+
+  const avatar1 = (await axios.get(`https://graph.facebook.com/${senderID}/picture?height=512&width=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
+  const avatar2 = (await axios.get(`https://graph.facebook.com/${partnerID}/picture?height=512&width=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" })).data;
+  const loveGif = (await axios.get("https://media.tenor.com/HEwHf4Zah5EAAAAd/anime-love.gif", { responseType: "arraybuffer" })).data;
+
+  fs.writeFileSync(__dirname + "/cache/user1.png", Buffer.from(avatar1, "utf-8"));
+  fs.writeFileSync(__dirname + "/cache/user2.png", Buffer.from(avatar2, "utf-8"));
+  fs.writeFileSync(__dirname + "/cache/love.gif", Buffer.from(loveGif, "utf-8"));
+
+  const attachments = [
+    fs.createReadStream(__dirname + "/cache/user1.png"),
+    fs.createReadStream(__dirname + "/cache/love.gif"),
+    fs.createReadStream(__dirname + "/cache/user2.png")
+  ];
+
+  const message = {
+    body: `💘 | LOVE CONNECTOR 3000 ACTIVATED...\n━━━━━━━━━━━━━━━\n💞 Aaj ki Jodi Tay Ho Chuki Hai:\n✨ ${name1} ❤️ ${name2}\n\n❤️‍🔥 Compatibility Level: ${compatibility}\n⚧️ Partner Gender: ${gender}\n\n⏳ Connecting Hearts...\n💌 Sending Love Signals...\n💫 Destiny Matched!\n\n❝ Pyaar wahi hota hai... jo achanak ho jaye ❞\n\n💍 Shubh ho tum dono ka milan!\n━━━━━━━━━━━━━━━`,
+    mentions: tagUsers,
+    attachment: attachments
+  };
+
+  return api.sendMessage(message, threadID, messageID);
+};
