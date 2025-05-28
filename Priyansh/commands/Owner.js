99cf66d0d090d9c206177781bced28db602d594f
Dev093235 <dev431140@gmail.com>
2025-05-28 18:55:01 +0530

    Update Owner.js

--- Priyansh/commands/Owner.js
+++ Priyansh/commands/Owner.js
@@ -1,47 +1,57 @@
+const axios = require('axios');
+
 module.exports.config = {
   name: "owner",
   version: "1.0.1",
   hasPermssion: 0,
   credits: "Rudra",
-  description: "no prefix",
-  commandCategory: "No command marks needed",
-  usages: "...",
-  cooldowns: 100,
+  description: "No prefix needed, responds to keywords",
+  commandCategory: "No prefix",
+  usages: "Just say 'owner' or 'rudra'",
+  cooldowns: 5,
 };
 
-module.exports.handleEvent = function({ api, event }) {
-  const { threadID, messageID, body } = event;
-  const react = body.toLowerCase();
+const imgs = [
+  // Tere diye hue
+  "https://i.imgur.com/RygyQj0.jpeg",
+  "https://i.imgur.com/lKtqjkH.jpeg",
+  "https://i.imgur.com/6a35Zrf.jpeg",
+  "https://i.imgur.com/CojcGcE.jpeg",
+  // Mere diye hue anime-style
+  "https://i.imgur.com/aLszk7Y.jpeg",
+  "https://i.imgur.com/8X2MQTT.jpeg",
+  "https://i.imgur.com/sV6tdbh.jpeg",
+  "https://i.imgur.com/Zhrl89g.jpeg",
+  "https://i.imgur.com/vwXCDP1.jpeg",
+  "https://i.imgur.com/qaC7UHo.jpeg"
+];
 
-  if (react.includes("owner") || react.includes("rudra")) {
-    const images = [
-      // 🧠 Your provided images
-      "https://i.imgur.com/RygyQj0.jpeg",
-      "https://i.imgur.com/lKtqjkH.jpeg",
-      "https://i.imgur.com/6a35Zrf.jpeg",
-      "https://i.imgur.com/CojcGcE.jpeg",
+module.exports.handleEvent = async function({ api, event }) {
+  try {
+    const { threadID, messageID, body } = event;
+    if (!body) return;
 
-      // 🔥 Anime-style Rudra images added
-      "https://i.imgur.com/mW5YWyB.jpeg", // anime girl + Rudra text
-      "https://i.imgur.com/NctYXm5.jpeg", // samurai style
-      "https://i.imgur.com/2KfG3CE.jpeg", // Tokyo theme
-      "https://i.imgur.com/rDqKuSA.jpeg", // glowing face
-      "https://i.imgur.com/1RkBe2y.jpeg", // night demon
-      "https://i.imgur.com/BRGF7kV.jpeg"  // red aesthetic
-    ];
+    const text = body.toLowerCase();
+    if (text.includes("owner") || text.includes("rudra")) {
+      const randomImage = imgs[Math.floor(Math.random() * imgs.length)];
 
-    // Random image
-    const randomImage = images[Math.floor(Math.random() * images.length)];
+      const response = await axios({
+        method: 'GET',
+        url: randomImage,
+        responseType: 'stream'
+      });
 
-    const msg = {
-      body:
-        "★ 𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦ 𝐑𝐮𝐝𝐫𝐚 ✦\n\n★★᭄ 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤:\n\n✦ https://youtube.com/@MirryKal ✦\n\n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽:\n𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋\nhttps://www.facebook.com/groups/207371140648761/?ref=share_group_link",
-      attachment: { url: randomImage }
-    };
+      const msg = {
+        body: `★ 𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦ 𝐑𝐮𝐝𝐫𝐚 ✦\n\n▶ Youtube: https://youtube.com/@MirryKal`,
+        attachment: response.data
+      };
 
-    api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", messageID, (err) => {}, true);
+      api.sendMessage(msg, threadID, messageID);
+      api.setMessageReaction("📷", messageID, () => {}, true);
+    }
+  } catch (err) {
+    console.log("❌ Error:", err.message);
   }
 };
 
-module.exports.run = function() {};
+module.exports.run = () => {};
