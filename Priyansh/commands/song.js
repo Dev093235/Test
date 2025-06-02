d2396b3800e0d69f55fe4088ea67ff5948a8adcd
Dev093235 <dev431140@gmail.com>
2025-06-02 13:58:14 +0530

    Update song.js

--- Priyansh/commands/song.js
+++ Priyansh/commands/song.js
@@ -5,7 +5,7 @@ const ytSearch = require("yt-search");
 
 module.exports = {
   config: {
-    name: "music",
+    name: "song",
     version: "1.0.3",
     hasPermssion: 0,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
