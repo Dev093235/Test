434641148cfa1110979f5c61f9087d3d27038c34
Dev093235 <dev431140@gmail.com>
2025-06-13 19:45:35 +0530

    Update coupledp.js

--- Priyansh/commands/coupledp.js
+++ Priyansh/commands/coupledp.js
@@ -1,49 +1,96 @@
+const axios = require("axios");
+
 module.exports.config = {
   name: "coupledp",
-  version: "1.0.1",
+  version: "1.0.4",
   hasPermssion: 0,
-  credits: "PREM BABU + Rudra 💘",
-  description: "Couple Dp photos (Pinterest-style)",
-  commandCategory: "Random-IMG",
+  credits: "Rudra",
+  description: "Stylish couple DPs — cinematic, Pinterest-style",
+  commandCategory: "fun",
   usages: "coupledp",
-  cooldowns: 2,
-  dependencies: {
-    "request": "",
-    "fs-extra": "",
-    "axios": ""
-  }
+  cooldowns: 3,
 };
 
-module.exports.run = async ({ api, event, args, Users, Threads, Currencies }) => {
-  const axios = global.nodemodule["axios"];
-  const request = global.nodemodule["request"];
-  const fs = global.nodemodule["fs-extra"];
-  var link = [
-    "https://i.imgur.com/VXY4Fxy.jpg", "https://i.imgur.com/wO9aM6S.jpg", "https://i.imgur.com/nQ7w5mA.jpg",
-    "https://i.imgur.com/qVblWRM.jpg", "https://i.imgur.com/h3iNUfa.jpg", "https://i.imgur.com/FNVuPgH.jpg",
-    "https://i.imgur.com/z4ffGUO.jpg", "https://i.imgur.com/B5sTULW.jpg", "https://i.imgur.com/Y7jDLa9.jpg",
-    "https://i.imgur.com/lA8ts2N.jpg", "https://i.imgur.com/5gb8bTI.jpg", "https://i.imgur.com/oeocHt3.jpg",
-    "https://i.imgur.com/4dTzZ7r.jpg", "https://i.imgur.com/Rwi0FYY.jpg", "https://i.imgur.com/1Lu3o2L.jpg",
-    "https://i.imgur.com/4WRDJSj.jpg", "https://i.imgur.com/Oz1Ek1n.jpg", "https://i.imgur.com/EI39BHe.jpg",
-    "https://i.imgur.com/FCLFc9T.jpg", "https://i.imgur.com/F9aPaU4.jpg", "https://i.imgur.com/5rEBx5m.jpg",
-    "https://i.imgur.com/jLx4qVG.jpg", "https://i.imgur.com/iwzLoB3.jpg", "https://i.imgur.com/5HkzYkA.jpg",
-    "https://i.imgur.com/BBFDDQI.jpg", "https://i.imgur.com/WIG0WVI.jpg", "https://i.imgur.com/ovHd3Z8.jpg",
-    "https://i.imgur.com/lKmA1Ex.jpg", "https://i.imgur.com/5sL3om7.jpg", "https://i.imgur.com/zwk0qYY.jpg",
-    "https://i.imgur.com/lUYR1wv.jpg", "https://i.imgur.com/fD0yPjA.jpg", "https://i.imgur.com/93LRaJ5.jpg",
-    "https://i.imgur.com/JPBywQu.jpg", "https://i.imgur.com/z3zkSQf.jpg", "https://i.imgur.com/v0KUmJJ.jpg",
-    "https://i.imgur.com/XapLU1W.jpg", "https://i.imgur.com/J3m9VuM.jpg", "https://i.imgur.com/SJb1IXU.jpg",
-    "https://i.imgur.com/mk9vA9O.jpg", "https://i.imgur.com/82uHTa6.jpg", "https://i.imgur.com/bAFIovg.jpg",
-    "https://i.imgur.com/ovCRD2g.jpg", "https://i.imgur.com/ezSBuRh.jpg", "https://i.imgur.com/8U0FL5T.jpg",
-    "https://i.imgur.com/zoFzZT9.jpg", "https://i.imgur.com/YzY6Xhw.jpg", "https://i.imgur.com/S1ezHsn.jpg",
-    "https://i.imgur.com/JChMT6L.jpg", "https://i.imgur.com/TuADzYI.jpg"
+// 🔒 Tamper Protection (Don't edit below)
+const expectedCredits = "Rudra";
+const expectedMessage = `💑 Here's your lovely *Couple DP* 💘\n\n🖤 Powered by Rudra`;
+
+if (
+  module.exports.config.credits !== expectedCredits ||
+  !expectedMessage.includes("Powered by Rudra")
+) {
+  throw new Error("🚫 Code tampered: Rudra protection active.");
+}
+
+module.exports.run = async function ({ api, event }) {
+  const links = [
+    "https://i.imgur.com/YsJ6EpW.jpg",
+    "https://i.imgur.com/kpUuTzN.jpg",
+    "https://i.imgur.com/JB8sLhD.jpg",
+    "https://i.imgur.com/KoNuCzO.jpg",
+    "https://i.imgur.com/wW7tJHk.jpg",
+    "https://i.imgur.com/pLbDUbl.jpg",
+    "https://i.imgur.com/tbmjaSG.jpg",
+    "https://i.imgur.com/GErpYoG.jpg",
+    "https://i.imgur.com/AxJYklx.jpg",
+    "https://i.imgur.com/ctSukqB.jpg",
+    "https://i.imgur.com/DPOueUv.jpg",
+    "https://i.imgur.com/dMdOQv3.jpg",
+    "https://i.imgur.com/4WFK4ep.jpg",
+    "https://i.imgur.com/vnMi7aH.jpg",
+    "https://i.imgur.com/1oQutlv.jpg",
+    "https://i.imgur.com/tK6UUXz.jpg",
+    "https://i.imgur.com/KHKsJ2j.jpg",
+    "https://i.imgur.com/h2Ae7pT.jpg",
+    "https://i.imgur.com/gzCiyZZ.jpg",
+    "https://i.imgur.com/z9cLepB.jpg",
+    "https://i.imgur.com/KI0RL4d.jpg",
+    "https://i.imgur.com/Xozdkfe.jpg",
+    "https://i.imgur.com/QzAeCrO.jpg",
+    "https://i.imgur.com/7Xw7FPG.jpg",
+    "https://i.imgur.com/Tcx7Y0D.jpg",
+    "https://i.imgur.com/g1S2jv5.jpg",
+    "https://i.imgur.com/NWUlQfH.jpg",
+    "https://i.imgur.com/YYZZVq4.jpg",
+    "https://i.imgur.com/SZ8Ttpx.jpg",
+    "https://i.imgur.com/kftkXZw.jpg",
+    "https://i.imgur.com/pDCo7hq.jpg",
+    "https://i.imgur.com/ekOhb3E.jpg",
+    "https://i.imgur.com/M7LjkNv.jpg",
+    "https://i.imgur.com/cg2L9fC.jpg",
+    "https://i.imgur.com/O9JhHms.jpg",
+    "https://i.imgur.com/JkK1Z9m.jpg",
+    "https://i.imgur.com/ko2vLpj.jpg",
+    "https://i.imgur.com/Eag5JBy.jpg",
+    "https://i.imgur.com/t5kzJjh.jpg",
+    "https://i.imgur.com/Ikhtv95.jpg",
+    "https://i.imgur.com/4AvRIok.jpg",
+    "https://i.imgur.com/J1zSUEj.jpg",
+    "https://i.imgur.com/04Dc9rp.jpg",
+    "https://i.imgur.com/lACXZ3S.jpg",
+    "https://i.imgur.com/ZLgvdRi.jpg",
+    "https://i.imgur.com/YwDkBT1.jpg",
+    "https://i.imgur.com/9N7roOY.jpg",
+    "https://i.imgur.com/1RBmIGL.jpg",
+    "https://i.imgur.com/TLsy7Uz.jpg",
+    "https://i.imgur.com/c9mRp2G.jpg",
+    "https://i.imgur.com/9wX60Ld.jpg",
+    "https://i.imgur.com/yy0xqgK.jpg",
+    "https://i.imgur.com/f2E9WaX.jpg",
+    "https://i.imgur.com/P48cdYD.jpg",
+    "https://i.imgur.com/dTyaQUd.jpg",
+    "https://i.imgur.com/VQL2XYm.jpg"
   ];
 
-  var callback = () => api.sendMessage({
-    body: `💑 Here's your lovely *Couple DP* 💘`,
-    attachment: fs.createReadStream(__dirname + "/cache/1.jpg")
-  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"));
-
-  return request(encodeURI(link[Math.floor(Math.random() * link.length)]))
-    .pipe(fs.createWriteStream(__dirname + "/cache/1.jpg"))
-    .on("close", () => callback());
+  const randomLink = links[Math.floor(Math.random() * links.length)];
+  try {
+    const res = await axios.get(randomLink, { responseType: "stream" });
+    return api.sendMessage({
+      body: expectedMessage,
+      attachment: res.data
+    }, event.threadID, event.messageID);
+  } catch (err) {
+    console.error("❌ Image fetch failed:", err);
+    return api.sendMessage("⚠️ DP load failed. Try again shortly.", event.threadID, event.messageID);
+  }
 };
